84bfdd6120f8db00009e37115cc98d3b5ca9cd49
RahulTamhankar <rahultamhankar12@gmail.com>
2025-03-16 02:51:39 -0400

    graph

--- src/main/java/graphs/topologicalSort/topologicalSortDFS.java
+++ src/main/java/graphs/topologicalSort/topologicalSortDFS.java
@@ -6,30 +6,14 @@ public class topologicalSortDFS {
     // Static Deque to store the topological order
     static Deque<Integer> dq = new LinkedList<>();
 
-    // DFS function to perform the depth-first search
-    public static void dfs(int node, boolean[] visited, List<List<Integer>> adj) {
-        visited[node] = true; // vsisted node to true mark kardiya
-        for (int nbr : adj.get(node)) { //nbr pe traverse karo aur unvisted hai to dfs chalao recursively
-            if (!visited[nbr]) {
-                dfs(nbr, visited, adj);
-            }
-        }
-        //### HERO OF THE QUESTION ###
-        //Almost done of while push node in front of deque
-        dq.push(node); // Add node to the front of deque when done with its descendants
-
-        //STEPS dfs(node)
-        //1) US node ko visited mark kiya
-        //2) Uske neighbors ko call kiya
-        //3) deque ke front me push kiya
-    }
 
     // Function to return the topological order as a list
     public static List<Integer> topologicalSort(List<List<Integer>> adj) {
         int n = adj.size();
         boolean[] visited = new boolean[n];
         // Perform DFS for every unvisited node
-        //iterate on all nodes and whenever we got a unvisted node , start dfs from that unvisted node, inside I am passing
+        //iterate on all nodes and whenever we got a unvisted node ,
+        // start dfs from that unvisted node, inside I am passing
         //the node, vsisited array and adjList
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
@@ -44,6 +28,27 @@ public class topologicalSortDFS {
         }
         return ans;
     }
+
+    // DFS function to perform the depth-first search
+    public static void dfs(int node, boolean[] visited, List<List<Integer>> adj) {
+        visited[node] = true; // vsisted node to true mark kardiya
+        for (int nbr : adj.get(node)) { //nbr pe traverse karo aur unvisted hai to dfs chalao recursively
+            if (!visited[nbr]) {
+                dfs(nbr, visited, adj);
+            }
+        }
+        //### HERO OF THE QUESTION ###
+        //Almost done of while push node in front of deque
+        //node ke sare nbrs khatam hogai vo time pe parent call pe return karne ke pehle dq.push kardo
+        dq.push(node); // Add node to the front of deque when done with its descendants
+
+        //STEPS dfs(node)
+        //1) US node ko visited mark kiya
+        //2) Uske neighbors ko call kiya
+        //3) deque ke front me push kiya
+    }
+
+
 
     public static void main(String[] args) {
         // Example usage
